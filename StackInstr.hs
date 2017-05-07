c3b9b35feddc20495285508259a869bcdac515e5
Pivotal <sandy.vanderbleek@gmail.com>
2017-05-07 13:32:14 -0400

    Check more cases for <>

--- StackInstr.hs
+++ StackInstr.hs
@@ -1,16 +1,9 @@
 module StackInstr where
 
--- | Stack evaluation
---
--- Examples:
---
--- >>> runStack [pushs ", really", pushs " years old", pushi 5, pushs "I am ", Add, Add, Add, Print]
--- ([DS "I am 5 years old, really"],[])
-
 data Data
   = DI Int
   | DS String
-  deriving (Show)
+  deriving (Show, Eq)
 
 data Instruction
   = Push Data
@@ -27,6 +20,13 @@ pushi = Push . DI
 
 type Effects = ([Data], [Data])
 
+-- | Stack evaluation
+--
+-- Examples:
+--
+-- >>> runStack [pushs ", really", pushs " years old", pushi 5, pushs "I am ", Add, Add, Add, Print]
+-- I am 5 years old, really
+
 runStack :: [Instruction] -> Effects
 runStack instructions =
   interate' instructions ([],[])
@@ -42,16 +42,35 @@ runStack' (out,stack) (Push d) =
 runStack' state@(_,[]) Pop = state
 runStack' (out,_:rest) Pop =
   (out,rest)
-runStack' (out, data1:data2:rest) Add =
-  (out, addData data1 data2 : rest)
+runStack' (out, d1:d2:rest) Add =
+  (out, (d1 <> d2):rest)
 runStack' stack Add =
   stack
 runStack' (out,(head:rest)) Print =
   (head:out, rest)
 runStack' state@(_,[]) Print = state
 
-addData :: Data -> Data -> Data
-addData (DI int1) (DI int2) = DI (int1 + int2)
-addData (DS str1) (DS str2) = DS (str1 ++ str2)
-addData (DI int1) (DS str2) = DS ((show int1) ++ str2)
-addData (DS str1) (DI int2) = DS (str1 ++ (show int2))
+-- | Data Combinations
+--
+-- Examples:
+--
+-- prop> \x y z -> (DI x <> DI y) <> DI z == DI x <> (DI y <> DI z)
+-- prop> \x y z -> (DS x <> DS y) <> DS z == DS x <> (DS y <> DS z)
+-- prop> \x y -> isDI (DI x <> DI y)
+-- prop> \x y -> isDS (DS x <> DS y)
+-- prop> \x y -> isDS (DS x <> DI y)
+-- prop> \x y -> isDS (DI x <> DS y)
+
+(<>) :: Data -> Data -> Data
+(DI int1) <> (DI int2) = DI (int1 + int2)
+(DS str1) <> (DS str2) = DS (str1 ++ str2)
+(DI int1) <> (DS str2) = DS ((show int1) ++ str2)
+(DS str1) <> (DI int2) = DS (str1 ++ (show int2))
+
+isDS :: Data -> Bool
+isDS (DI _) = False
+isDS (DS _) = True
+
+isDI :: Data -> Bool
+isDI (DS _) = False
+isDI (DI _) = True
